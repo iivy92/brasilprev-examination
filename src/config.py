967026934230737c90a5705924b4c7f4da52b986
Pedro Santana <pedro.santana@ilia.digital>
2024-05-08 01:52:47 -0300

    adding env file

---
+++ src/config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+QUANTITY_OF_PROPERTIES = os.getenv("QUANTITY_OF_PROPERTIES")
+NUMBER_SIMULATIONS = os.getenv("NUMBER_SIMULATIONS")
+TIMEOUT_ROUND = os.getenv("TIMEOUT_ROUND")
+PLAYER_MONEY = os.getenv("PLAYER_MONEY")
+PLAYER_MONEY_ROUND = os.getenv("PLAYER_MONEY_ROUND")
