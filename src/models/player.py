6c4b7eaf4aaf4a8b784dc88fd378ab163da735ae
Pedro Santana <pedro.santana@ilia.digital>
2024-05-07 23:20:11 -0300

    addiing player features

--- src/models/player.py
+++ src/models/player.py
@@ -7,8 +7,34 @@ class PlayerStrategy(str, Enum):
     CAUTIOUS = "Cautious"
     RANDOM = "Random"
 
+
 class Player(BaseModel):
     money: int = 300
     position: int = 0
-    strategy: str
+    strategy: PlayerStrategy
     gameover: bool = False
+
+
+    def verify_property(self, property, board=None):
+        if property.owner:
+            if self != property.owner:
+                self.pay_rent(property)
+            return
+
+        if self._decide_to_buy_property():
+            property.owner = self
+
+    def pay_rent(self, property):
+        self.money -= property.rent_price
+    
+    def _decide_to_buy_property(self, property):
+        if self.strategy == PlayerStrategy.IMPULSIVE:
+            return True
+        elif self.strategy == PlayerStrategy.DEMANDING:
+            return property.rent > 50
+        elif self.strategy == PlayerStrategy.CAUTIOUS:
+            return self.money >= property.cost + 80
+        elif self.strategy == PlayerStrategy.RANDOM:
+            return random.choice([True, False])
+        
+        return False
--- src/models/property.py
+++ src/models/property.py
@@ -5,5 +5,5 @@ from src.models.player import Player
 class Property(BaseModel):
     id: int
     owner: Player = None
-    cost: int = random.randint(20, 110)
-    rent: int = random.randint(20, 110)
+    price: int = random.randint(20, 110)
+    rent_price: int = price/2
