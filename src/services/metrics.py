db318110baa12da8ebccb08baf59ed28efa469e8
Pedro Santana <pedro.santana@ilia.digital>
2024-05-09 13:39:29 -0300

    adding metrics module

---
+++ src/services/metrics.py
@@ -0,0 +1,15 @@
+from src.models.board import Board
+
+class HandlerMetrics:
+    def __init__(self):
+        self.rounds = []
+    
+    def save_metrics(self, board_game: Board):
+        metrics = {
+            "timed_out": board_game.timed_out,
+            "turns": board_game.plays,
+            "winner": board_game.winner.strategy,
+        }
+        
+        self.rounds.append(board_game)
+    
